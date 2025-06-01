9e16530a382edccd63d15dd6a0a6d440ba905a40
Manasyu Chaudhari <manasyu.chaudhari@gmail.com>
2025-06-01 16:57:41 -0500

    removed unused output

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -2,7 +2,3 @@ output "bastion_public_ip" {
   value = length(aws_instance.bastion) > 0 ? aws_instance.bastion[0].public_ip : null
 
 }
-
-output "bastion_sg_id" {
-    value = length(aws_instance.bastion) > 0 ? aws_security_group.bastion_sg.id : null
-}
